cf78835e8f375a33faeb087420d7491cb335b3c9
Muhammad Faheem <titanrulez@gmail.com>
2022-10-02 21:19:12 +0200

    Added unit tests for signup route

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "abcdefg", password: "password" })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "1" })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "", password: "password" })
+    .expect(400);
+
+    await request(app)
+      .post("/api/users/signup")
+      .send({ email: "", password: "" })
+      .expect(400);
+});
