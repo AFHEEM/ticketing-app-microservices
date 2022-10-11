bbc2f2c7fc365e3f1dffa60824fda60f0cea69dd
Muhammad Faheem <titanrulez@gmail.com>
2022-10-11 19:50:44 +0200

    Added unit tests for signout, signin and signup functionality

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -16,7 +16,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true, // enable only https for cookies
+    secure: process.env.NODE_ENV !== "test", // enable only https for cookies
   })
 );
 
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -33,8 +33,29 @@ it("returns a 400 with missing email and password", async () => {
     .send({ email: "", password: "password" })
     .expect(400);
 
-    await request(app)
-      .post("/api/users/signup")
-      .send({ email: "", password: "" })
-      .expect(400);
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "", password: "" })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(400);
+});
+
+it("sets a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
 });
